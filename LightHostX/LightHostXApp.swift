a12ec628f67239c60b0f623157d03055bce759dc
Sergey Aleynikov <sergey.aleynikov@gmail.com>
2024-08-09 23:12:17 +0300

    new project

---
+++ LightHostX/LightHostXApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightHostXApp.swift
+//  LightHostX
+//
+//  Created by dur-randir on 09.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct LightHostXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
